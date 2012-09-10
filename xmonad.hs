5d8f5914c7c67aefb471154741b09431d95512ce
Bjørn Uhre Arnholtz <bua@unisport.dk>
2012-09-10 09:27:22 +0200

    Yo

--- xmonad.hs
+++ xmonad.hs
@@ -112,7 +112,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm,               xK_h     ), sendMessage Shrink)
 
     -- Expand the master area
-    , ((modm,               xK_l     ), sendMessage Expand)
+    -- , ((modm,               xK_l     ), sendMessage Expand)
 
     -- Push window back into tiling
     , ((modm,               xK_t     ), withFocused $ windows . W.sink)
